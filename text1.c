35003393efe198be43472b4c00a8c5b9ee57dc70
zxl-0213 <287907757@qq.com>
2021-03-15 22:36:50 +0800

    1、最大高度 2、翻转二叉树 3、相同的树

---
+++ text1.c
@@ -0,0 +1,19 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+
+int maxDepth(struct TreeNode* root){
+    if(root==NULL)
+    {
+        return 0;
+    }
+    int leftdepth=1+maxDepth(root->left);
+    int rightdepth=1+maxDepth(root->right);
+    return leftdepth>rightdepth?leftdepth:rightdepth;
+}
